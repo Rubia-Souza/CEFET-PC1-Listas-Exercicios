a69d350355918be9a57f929bbd505e022ad1035b
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-10-28 11:39:17 -0300

    feat: Adiciona atividades da lista 1 do uri

---
+++ src/CEFET-PC1-Lista01/produtoSimples/produtoSimples.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 0, b = 0;
+
+    cin >> a;
+    cin >> b;
+
+    int prod = a * b;
+
+    cout << "PROD = " << prod << "\n";
+    return 0;
+}
