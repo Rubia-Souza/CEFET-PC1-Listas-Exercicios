aef1fc0d679bfba025290e86b26169dc2cad5233
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-12-09 14:54:39 -0300

    feat(lista6): Implementa exercicio matriz quadrada 2
    presentation erro n sei pq

---
+++ src/CEFET-PC1-Lista06/MatrizQuadrada2/matrizQuadrada2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void buildMatrix(const int order);
+void printMatrix(const vector<vector<int>> matrix, const int order);
+
+int main() {
+    vector<int> matrixOrders;
+    int order = 0;
+
+    do {
+        do {
+            cin >> order;
+        } while (order < 0 || order > 100);
+
+        matrixOrders.push_back(order);
+    } while (order != 0);
+
+    for (int i = 0; i < matrixOrders.size(); i++) {
+        buildMatrix(matrixOrders[i]);
+        if (i < matrixOrders.size() - 1) {
+            cout << "\n";
+        }
+    }
+    
+    return 0;
+}
+
+void buildMatrix(const int order) {
+    vector<vector<int>> matrix(order, vector<int>(order));
+
+    for (int i = 0; i < order; i++) {
+        int value = i + 2;
+        for (int j = 0; j < order; j++) {
+            if (i > j) {
+                value--;
+            }
+            else if (i == j) {
+                value = 1;
+            }
+            else if (i < j) {
+                value++;
+            }
+
+            matrix[i][j] = value;
+        }
+    }
+
+    printMatrix(matrix, order);
+    return;
+}
+
+void printMatrix(const vector<vector<int>> matrix, const int order) {
+    for (int i = 0; i < order; i++) {
+        cout << "  ";
+
+        for (int j = 0; j < order; j++) {
+            cout << matrix[i][j];
+            if (j < order - 1) {
+                cout << "   ";
+            }
+        }
+
+        cout << "\n";
+    }
+
+    return;
+}
