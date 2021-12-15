501d4292796e796c77bb92c84d639a4b8a8cdc79
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-12-15 11:39:50 -0300

    fix: Ajusta exercício aliteração

--- src/CEFET-PC1-Lista07/aliteracao/aliteracao.cpp
+++ src/CEFET-PC1-Lista07/aliteracao/aliteracao.cpp
@@ -3,42 +3,53 @@
 
 using namespace std;
 
+string getFirstLetters(const string line);
 int countAlitarations(const string line);
 
 int main() {
-    do {
-        string lineToTest = "";
-        getline(cin, lineToTest);
+    string lineToTest = "";
 
+    while (getline(cin, lineToTest)) {
         int amountAlitararions = countAlitarations(lineToTest);
         cout << amountAlitararions << "\n";
-    } while(!cin.eof());
+    }
 
     return 0;
 }
 
+string getFirstLetters(const string line) {
+    string result = "";
+    bool foundFirstCharacter = false;
+
+    for (int i = 0; i < line.length(); i++) {
+        if (isalpha(line[i]) && !foundFirstCharacter) {
+            result.push_back(tolower(line[i]));
+            foundFirstCharacter = true;
+        }
+        else if(line[i] == ' ') {
+            foundFirstCharacter = false;
+        }
+    }
+
+    return result;
+}
+
 int countAlitarations(const string line) {
+    string firstLetters = getFirstLetters(line);
     int count = 0;
     char lastLetter = ' ';
     bool hasAlreadyIncremented = false;
-    bool hasFoundFirstCharacter = false;
 
-    for (int i = 0; i < line.length(); i++) {
-        char actualLetter = tolower(line[i]);
-        bool isTheFirstLetter = isalpha(actualLetter) && !hasFoundFirstCharacter;
-        if (isTheFirstLetter) {
-            hasFoundFirstCharacter = true;
-            if (actualLetter != lastLetter) {
-                hasAlreadyIncremented = false;
-            }
-            else if (!hasAlreadyIncremented) {
-                count++;
-                hasAlreadyIncremented = true;
-            }
+    for (int i = 0; i < firstLetters.length(); i++) {
+        char actualCharacter = firstLetters[i];
+        if (lastLetter != actualCharacter) {
+            hasAlreadyIncremented = false;
         }
-        else if (actualLetter == ' ') {
-            hasFoundFirstCharacter = false;
+        else if (lastLetter == actualCharacter && !hasAlreadyIncremented) {
+            hasAlreadyIncremented = true;
+            count++;
         }
+        lastLetter = actualCharacter;
     }
 
     return count;
